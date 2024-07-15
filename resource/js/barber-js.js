2e723c6812c45d1c9e3cd460f8e2862a624fdc4e
BhavikShahofficial <bhavikshahofficial@gmail.com>
2024-07-15 13:59:59 +0530

    Responsive + Features

--- resource/js/barber-js.js
+++ resource/js/barber-js.js
@@ -1,14 +1,39 @@
 var menubtn = document.getElementById("menubtn");
 var sidenav = document.getElementById("sidenav");
 var menu = document.getElementById("menu");
-sidenav.style.right = "-250px";
+
+var defaultWidth = 250;
+var expandedWidth = 750;
+
+// Function to set sidenav width based on window width
+function setSidenavWidth() {
+  if (window.innerWidth > 1920) {
+    sidenav.style.width = expandedWidth + "px";
+    if (sidenav.style.right === "0px") {
+      sidenav.style.right = "-" + expandedWidth + "px";
+    }
+  } else {
+    sidenav.style.width = defaultWidth + "px";
+    if (sidenav.style.right === "0px") {
+      sidenav.style.right = "-" + defaultWidth + "px";
+    }
+  }
+}
+
+// Initial setup
+setSidenavWidth();
+
+// Event listener for resizing
+window.onresize = setSidenavWidth;
+
+sidenav.style.right = "-" + sidenav.style.width;
 
 menubtn.onclick = function () {
-  if (sidenav.style.right == "-250px") {
+  if (sidenav.style.right === "-" + sidenav.style.width) {
     sidenav.style.right = "0";
     menu.src = "images/barber/close.png";
   } else {
-    sidenav.style.right = "-250px";
+    sidenav.style.right = "-" + sidenav.style.width;
     menu.src = "images/barber/menu.png";
   }
 };
